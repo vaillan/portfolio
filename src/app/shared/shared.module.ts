a00c7af12a1b345002642ea6010ab7508e518f38
valentin-ortiz <vortiz@capi.lat>
2022-12-03 14:47:54 -0600

    Actualización

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThreeGlobeComponent } from './three-globe/three-globe.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
-import {BreadcrumbModule} from 'xng-breadcrumb';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
-
-
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
     ThreeGlobeComponent,
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    LoadingComponent
   ],
   imports: [
     CommonModule,
@@ -19,7 +19,8 @@ import { AngularMaterialModule } from '../angular-material/angular-material.modu
   ],
   exports: [
     ThreeGlobeComponent,
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    LoadingComponent
   ]
 })
 export class SharedModule { }
